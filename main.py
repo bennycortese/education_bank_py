9c608ac8545f26448bb0dc217eb708d2d5a74566
pizza654321 <bennycortese@gmail.com>
2023-09-03 00:21:49 -0400

    Used the Summarize each block of text method to get around character limit, can now generate questions even if there is too much text by feeding the text into summaries and then using that to generate the question bank

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import openai
 import os
 from dotenv import load_dotenv
 
-def print_hi(textbook_text):
+
+def write_questions(textbook_text):
     # Use a breakpoint in the code line below to debug your script.
     openai.organization = "org-1EAYlJSwfnMNuS2Z6d6I4cNU"
     load_dotenv()
@@ -10,21 +11,18 @@ def print_hi(textbook_text):
     openai.Model.list()
     prompt_messages = []
     prompt_messages.append({"role": "system", "content": "You are an instructor designing new course content."})
-    prompt_messages.append({"role": "user", "content": "I am going to feed you textbook data and I want you to read it without saying anything. Say yes if you understand"})
+    prompt_messages.append({"role": "user",
+                            "content": "I am going to feed you textbook data and I want you to read it without saying anything. Say yes if you understand"})
     for text_chunk in textbook_text:
         prompt_messages.append({"role": "user", "content": text_chunk})
     prompt_messages.append({"role": "user", "content": "Ok that is all the textbook data, say yes if you understand"})
-    prompt_messages.append({"role": "user", "content": "Write 5 questions from what you read that require a combination of critical thinking and synthesis, put them in a multiple choice format and then list the answers"})
+    prompt_messages.append({"role": "user",
+                            "content": "Write 5 questions from what you read that require a combination of critical thinking and synthesis, put them in a multiple choice format and then list the answers"})
     completion = openai.ChatCompletion.create(
-        model="gpt-4-32k",
+        model="gpt-3.5-turbo-0301",
         messages=prompt_messages
     )
 
-    #{"role": "system", "content": "You are an instructor designing new course content."},
-            #{"role": "user", "content": "I am going to feed you textbook data and I want you to read it without saying anything. Say yes if you understand"},
-            #{"role": "user", "content": textbook_text},
-            #{"role": "user", "content": "Ok that is all the textbook data, say yes if you understand"},
-            #{"role": "user", "content": "Write 5 questions from what you read that require a combination of critical thinking and synthesis, put them in a multiple choice format and then list the answers"}
     print(completion.choices[0].message)
 
 
@@ -33,16 +31,47 @@ def textbook_scrape(textbook_file_name):
     line = f.read()
     n = 4096
     return [line[i:i + n] for i in range(0, len(line), n)]
-    return f.read()
 
+
+def summary_extraction(textbook_text):
+    text_arrays = []
+    summarized_text = []
+    for i in range(0, len(textbook_text), 3):
+        text_arrays.append([textbook_text[i], textbook_text[i + 1], textbook_text[i + 2]])
+    for text_array in text_arrays:
+        summarized_text.append(summarize_text(text_array))
+    return summarized_text
+
+
+def summarize_text(textbook_text):
+    openai.organization = "org-1EAYlJSwfnMNuS2Z6d6I4cNU"
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    prompt_messages = []
+    prompt_messages.append(
+        {"role": "system", "content": "You are an intelligent reader who is good at summarizing text."})
+    prompt_messages.append({"role": "user",
+                            "content": "I am going to feed you textbook data and I want you to read it without saying anything. Say yes if you understand."})
+    for text_chunk in textbook_text:
+        prompt_messages.append({"role": "user", "content": text_chunk})
+    prompt_messages.append({"role": "user", "content": "Ok that is all the textbook data, say yes if you understand"})
+    prompt_messages.append({"role": "user",
+                            "content": "Write a summary of no more than 500 words from that data, focusing on what seems to be the most important content"})
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0301",
+        messages=prompt_messages
+    )
+    return completion.choices[0].message['content']
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi(textbook_scrape("ch1PrinciplesSysDesign.txt"))
-    #for line in textbook_scrape("ch1PrinciplesSysDesign.txt"):
+    #print(textbook_scrape("ch1PrinciplesSysDesign.txt"))
+    summaries = summary_extraction(textbook_scrape("ch1PrinciplesSysDesign.txt"))
+    write_questions(summaries)
+    # for line in textbook_scrape("ch1PrinciplesSysDesign.txt"):
     #    print(line)
-    #textbook_scrape("ch1IntroGameTheory.txt")
-    #textbook_scrape("w.txt")
+    # textbook_scrape("ch1IntroGameTheory.txt")
+    # textbook_scrape("w.txt")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
